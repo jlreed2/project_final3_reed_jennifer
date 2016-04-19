2d810846dfc2206bc03466eaf03b4fe2546d1c80
Jennifer Reed <jlreed2@ufl.edu>
2016-04-18 22:33:00 -0700

    Add custom jQuery - Dish of the day
    Code gets day of the week, and places a new <p> under the selected element
    using if/else statements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $( document ).ready( function( ) {
 
- //backstretch plugin:
-  $.backstretch( './img/brick_and_plaster_texture.png' );
+	//Custom jQuery:
+	var dish;
+	var day = new Date().getDay();
+
+	if ( day === 0 ) {
+	    dish = 'Leaky House Soup';
+	} else if ( day == 1 ) {
+	    dish = 'Game Pie';
+	} else if ( day == 2 ) {
+		dish = 'Roast Hog';
+	} else if ( day == 4 ) {
+		dish = 'Eccles Cake';
+	} else if ( day == 6 ) {
+		dish = 'Bangers and Mash';
+	} else {
+	    dish = 'Gamp\'s Old Gregarious';
+	}
+	$( '#deal' ).after( dish ) ;
+
+	 //Backstretch plugin:
+	$.backstretch( './img/brick_and_plaster_texture.png' );
 } );
